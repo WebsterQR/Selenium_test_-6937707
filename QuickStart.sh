fddaa94d79eeae9e3af20371c5cdfbd37fd5669d
KoverevEvgeniy <koverev.evg@gmail.com>
2023-06-30 01:40:08 +0300

    update requirements and gitignore, add bash script to lauch

---
+++ QuickStart.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+echo 'Hello World'
+allureversion=`allure --version`
+regexp="^(([1-9]|([0-4]|1\d|[1-9]|)\d)\.?\b){3}$"
+[["$allureversion" =~ $regexp ]] && need_install="NO" || need_install="YES"
+echo $allureversion
+echo $need_install
+if [[ $need_install=='YES' ]]
+  case "$OSTYPE" in
+    darwin*)  echo "OSX"; ;;
+    linux*)   echo "LINUX"; sudo apt-get install allure;;
+    msys*)    echo "WINDOWS"; scoop install allure ;;
+    *)        echo "unknown: $OSTYPE" ;;
+  esac
+fi
+#pytest -p no:warnings --showlocals tests --alluredir=allurereports;
+#allure serve allurereports;
+
+#  brew install allure
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    browser = webdriver.Chrome("/Users/evgeniy/Documents/Workspace/SimbirSoft_test/Selenium_test_-6937707/chromedriver")
+    browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
     browser.quit()
